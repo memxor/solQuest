d6f58d4e6137ec5b982040f6dbbf10ab93137b6e
memxor <bhattacharyakun@gmail.com>
2024-01-08 19:22:23 +0530

    Accepting Quests

--- tests/sol-quest.ts
+++ tests/sol-quest.ts
@@ -7,6 +7,25 @@ describe("sol-quest", () => {
   anchor.setProvider(provider);
 
   const program = anchor.workspace.SolQuest as Program<SolQuest>;
+  const [adminAccountPDA] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("Admin"), provider.publicKey.toBuffer()], program.programId);
+
+  it("Initialize Admin!", async () =>
+  {
+
+    await program.methods
+      .initializeAdmin()
+      .accounts({
+        signer: provider.publicKey,
+        admin: adminAccountPDA,
+        systemProgram: anchor.web3.SystemProgram.programId
+      })
+      .rpc();
+    
+    const adminAccount = await program.account.admin.fetch(adminAccountPDA);
+    
+    console.log(adminAccount.authority.toString());
+    console.log(adminAccount.matesSubmitted);
+  });
 
   it("Initialize Mate!", async () =>
   {
@@ -56,57 +75,88 @@ describe("sol-quest", () => {
     const [mateAccountPDA] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("Mate"), provider.publicKey.toBuffer()], program.programId);
 
     await program.methods
-      .addCompletedQuest({id: 1, deployedUrl: "github.com/memxor/example", transaction: "dfh9pauwfbhiuohbfiuPHNOUIHNOIPHFNDOSIFNOnsonfosinfo[s'fnosifnsofnin'fnd"})
-      .accounts({
-        signer: provider.publicKey,
-        user: mateAccountPDA,
-        systemProgram: anchor.web3.SystemProgram.programId
-      })
-      .rpc();
-    
-        await program.methods
-      .addCompletedQuest({id: 2, deployedUrl: "github.com/memxor/example", transaction: "dfh9pauwfbhiuohbfiuPHNOUIHNOIPHFNDOSIFNOnsonfosinfo[s'fnosifnsofnin'fnd"})
-      .accounts({
-        signer: provider.publicKey,
-        user: mateAccountPDA,
-        systemProgram: anchor.web3.SystemProgram.programId
-      })
-      .rpc();
-    
-        await program.methods
-      .addCompletedQuest({id: 3, deployedUrl: "github.com/memxor/example", transaction: "dfh9pauwfbhiuohbfiuPHNOUIHNOIPHFNDOSIFNOnsonfosinfo[s'fnosifnsofnin'fnd"})
-      .accounts({
-        signer: provider.publicKey,
-        user: mateAccountPDA,
-        systemProgram: anchor.web3.SystemProgram.programId
-      })
-      .rpc();
-    
-        await program.methods
-      .addCompletedQuest({id: 4, deployedUrl: "github.com/memxor/example", transaction: "dfh9pauwfbhiuohbfiuPHNOUIHNOIPHFNDOSIFNOnsonfosinfo[s'fnosifnsofnin'fnd"})
-      .accounts({
-        signer: provider.publicKey,
-        user: mateAccountPDA,
-        systemProgram: anchor.web3.SystemProgram.programId
-      })
-      .rpc();
-    
-        await program.methods
-      .addCompletedQuest({id: 5, deployedUrl: "github.com/memxor/example", transaction: "dfh9pauwfbhiuohbfiuPHNOUIHNOIPHFNDOSIFNOnsonfosinfo[s'fnosifnsofnin'fnd"})
-      .accounts({
-        signer: provider.publicKey,
-        user: mateAccountPDA,
-        systemProgram: anchor.web3.SystemProgram.programId
-      })
-      .rpc();
+    .addCompletedQuest(1, "github.com/memxor/example", "dfh9pauwfbhiuohbfiuPHNOUIHNOIPHFNDOSIFNOnsonfosinfo[s'fnosifnsofnin'fnd")
+    .accounts({
+      signer: provider.publicKey,
+      user: mateAccountPDA,
+      admin: adminAccountPDA,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).rpc();
+  
+    await program.methods
+    .addCompletedQuest(2, "github.com/memxor/example", "dfh9pauwfbhiuohbfiuPHNOUIHNOIPHFNDOSIFNOnsonfosinfo[s'fnosifnsofnin'fnd")
+    .accounts({
+      signer: provider.publicKey,
+      user: mateAccountPDA,
+      admin: adminAccountPDA,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).rpc();
+  
+    await program.methods
+    .addCompletedQuest(3, "github.com/memxor/example", "dfh9pauwfbhiuohbfiuPHNOUIHNOIPHFNDOSIFNOnsonfosinfo[s'fnosifnsofnin'fnd")
+    .accounts({
+      signer: provider.publicKey,
+      user: mateAccountPDA,
+      admin: adminAccountPDA,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).rpc();
+  
+    await program.methods
+    .addCompletedQuest(4, "github.com/memxor/example", "dfh9pauwfbhiuohbfiuPHNOUIHNOIPHFNDOSIFNOnsonfosinfo[s'fnosifnsofnin'fnd")
+    .accounts({
+      signer: provider.publicKey,
+      user: mateAccountPDA,
+      admin: adminAccountPDA,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).rpc();
+  
+    await program.methods
+    .addCompletedQuest(5, "github.com/memxor/example", "dfh9pauwfbhiuohbfiuPHNOUIHNOIPHFNDOSIFNOnsonfosinfo[s'fnosifnsofnin'fnd")
+    .accounts({
+      signer: provider.publicKey,
+      user: mateAccountPDA,
+      admin: adminAccountPDA,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).rpc();
     
     const mateAccount = await program.account.mate.fetch(mateAccountPDA);
+    const adminAccount = await program.account.admin.fetch(adminAccountPDA);
     
     console.log(mateAccount.authority.toString());
     console.log(mateAccount.mateNft.toString());
     console.log(mateAccount.mateJoinedDate.toNumber());
     console.log(mateAccount.mateRole);
     console.log(mateAccount.questCompletedByMate);
+
+    console.log(adminAccount.matesSubmitted);
+  });
+
+  it("Approve mate quest!", async () => {
+    const [mateAccountPDA] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("Mate"), provider.publicKey.toBuffer()], program.programId);
+
+    await program.methods
+    .approveUserQuest(1)
+    .accounts({
+      signer: provider.publicKey,
+      user: mateAccountPDA,
+      admin: adminAccountPDA,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).rpc();
+    
+    await program.methods
+    .approveUserQuest(2)
+    .accounts({
+      signer: provider.publicKey,
+      user: mateAccountPDA,
+      admin: adminAccountPDA,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).rpc();
+    
+    const mateAccount = await program.account.mate.fetch(mateAccountPDA);
+    const adminAccount = await program.account.admin.fetch(adminAccountPDA);
+
+    console.log(mateAccount.questCompletedByMate);
+    console.log(adminAccount.matesSubmitted);
   });
 
 });
